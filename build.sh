b1e7eb0891ca5a48a35dbd6bb5227f4dd87c1025
xujiang <realjiangxu@gmail.com>
2018-11-16 08:46:13 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+mvn -B -pl ambari-server clean install  package rpm:rpm -DnewVersion=2.7.0.0.0 -Dstack.distribution=JDP -DbuildNumber=631319b00937a8d04667d93714241d2a0cb17275 -DskipTests -Dpython.ver="python >= 2.6" -Drat.skip
+
+mvn -B -pl ambari-agent clean install  package rpm:rpm -DnewVersion=2.7.0.0.0 -Dstack.distribution=JDP -DbuildNumber=631319b00937a8d04667d93714241d2a0cb17275 -DskipTests -Dpython.ver="python >= 2.6" -Drat.skip
+
